ccd641a5f15721decf413b01c3385e20b0981205
willsmith9182 <willsmith182@gmail.com>
2015-04-23 00:25:18 +0100

    00

--- PipelineFramework.Tests/PipelineDiscoveryTests.cs
+++ PipelineFramework.Tests/PipelineDiscoveryTests.cs
@@ -1,9 +1,31 @@
 ﻿using NUnit.Framework;
+using PipelinePlusPlus.Attributes;
+using PipelinePlusPlus.Core;
 
 namespace PipelineFramework.Tests
 {
     [TestFixture]
-    public  class PipelineDiscoveryTests
+    public class PipelineDiscoveryTests
     {
+        internal class DiscoveryTestContext
+        : PipelineContext
+        {
+
+        }
+        internal class DiscoveryTestPipeline : PipelineSteps
+        {
+            public DiscoveryTestPipeline()
+                : base("DiscoveryTests")
+            {
+            }
+
+            [PipelineStep(0)]
+            public PipelineStep<DiscoveryTestContext> TestStep1 { get; set; }
+            [PipelineStep(1)]
+            public PipelineStep<DiscoveryTestContext> TestStep2 { get; set; }
+            [PipelineStep(2)]
+            public PipelineStep<DiscoveryTestContext> TestStep3 { get; set; }
+        }
+
     }
 }
